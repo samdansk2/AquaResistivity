3c1ab7970c82f476ea2cea510b645d7b9f7af486
samdansk2 <samdanshaik8998@gmail.com>
2024-01-31 21:12:57 +0530

    first commit

--- src/py_package/calculation.py
+++ src/py_package/calculation.py
@@ -1,17 +1,14 @@
 # Python program to find the resistivity of sea water for a given salinity and temperature.
 def resistivity(salinity,temperature):
-    base_conductance = 1.0
-    alpha = 0.0191  
-    beta = 0.00625  
-
-    sp_cond = base_conductance * (1 + alpha * salinity - beta * (temperature - 25))
     
-    resistivity = 1 / sp_cond
+    sp_conductance_from_table = 0.038065
+    
+    resistivity = 1 / sp_conductance_from_table
     
     return resistivity
 
-salinity = int(input(" enter salinity value in (ppt)"))
-temperature =int(input("enter temperature in (°C)"))
+salinity = 15
+temperature = 20 
 
 result = resistivity(salinity,temperature)
 
