d042a8fc256f8ba6e0158d5e2aabd0f16e0e154b
samdansk2 <samdanshaik8998@gmail.com>
2024-01-31 16:16:45 +0530

    first commit

--- src/py_package/calculation.py
+++ src/py_package/calculation.py
@@ -1,3 +1,18 @@
-def pressure_pipe():
-    print(10)
-    return 10
+# Python program to find the resistivity of sea water for a given salinity and temperature.
+def resistivity(salinity,temperature):
+    base_conductance = 1.0
+    alpha = 0.0191  
+    beta = 0.00625  
+
+    sp_cond = base_conductance * (1 + alpha * salinity - beta * (temperature - 25))
+    
+    resistivity = 1 / sp_cond
+    
+    return resistivity
+
+salinity = int(input(" enter salinity value in (ppt)"))
+temperature =int(input("enter temperature in (°C)"))
+
+result = resistivity(salinity,temperature)
+
+print("The Resistivity for the given salinity and temperature is " ,result)
