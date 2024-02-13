50b1176111c400dbbaf84f93d23d10ca0db79a9a
samdansk2 <samdanshaik8998@gmail.com>
2024-02-13 16:16:26 +0530

    testing

--- src/seawater_resistivity/tests/test_calculation.py
+++ src/seawater_resistivity/tests/test_calculation.py
@@ -1,4 +1,14 @@
-import py_package
-from py_package import calculation
+import seawater_resistivity
+import pytest
 
-calculation.pressure_pipe()
+def test_resistivity_calculation():
+    assert seawater_resistivity.resistivity(35, 25) == 18.855  # result: 18.855 Ohm-m
+
+def test_resistivity_negative_temperature():
+    # negative temperature
+    with pytest.raises(ValueError):
+        seawater_resistivity.resistivity(35, -5)  #raise ValueError
+
+def test_resistivity_zero_salinity():
+    # zero salinity
+    assert seawater_resistivity.resistivity(0, 25) == 4.943  # result: 4.943 Ohm-m
