d5a21489e9a09740912e7c6f98ef22a737d8a465
samdansk2 <samdanshaik8998@gmail.com>
2024-02-23 11:33:33 +0530

    test

--- src/seawater_resistivity/tests/test_seawater_resistivity.py
+++ src/seawater_resistivity/tests/test_seawater_resistivity.py
@@ -1,16 +1,20 @@
 import pytest
-from seawater_resistivity.seawater_resistivity_csv1 import calculate_resistivity
+from seawater_resistivity_csv1 import calculate_resistivity
 
 def test_calculation_with_known_input():
     # Test calculation 
-    expected_result= 66.3614042
+    expected_result= 66.361
     result = calculate_resistivity(10,0.7)
-    assert round(result, 7) == round(expected_result, 7)  
+    assert round(result, 3) == round(expected_result, 3)  
     
-
 def test_zero_salinity():
-    expected_result= 51.9885625
+    expected_result= 51.988
     result = calculate_resistivity(15,0.8)
-    assert round(result, 7) == round(expected_result, 7)
+    assert round(result, 3) == round(expected_result, 3)
+def test_random():
+    expected_result= 29.024
+    result = calculate_resistivity(5,2)
+    assert round(result, 3) == round(expected_result, 3)
+
 
 
