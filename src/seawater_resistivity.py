87efece56207dc7e0d45e7f70c64ba9c994ffba5
samdansk2 <samdanshaik8998@gmail.com>
2024-06-07 11:48:20 +0530

    ok

--- src/seawater_resistivity.py
+++ src/seawater_resistivity.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.interpolate import griddata
 
 df = pd.read_csv("src/seawater/input_data.csv")
 
@@ -10,7 +11,14 @@ def calculate_resistivity(temperature, salinity):
     value = df[(df['Temperature (deg C)'] == temperature) & (df['Salinity (%)'] == salinity)]
     
     if value.empty:
-        conductance = value['Conductance 1/(ohm.cm)'].mean()
+        points = df[['Temperature (deg C)', 'Salinity (%)']].values
+        values = df['Conductance 1/(ohm.cm)'].values
+        conductance = griddata(points, values, (temperature, salinity), method='linear')
+        
+        # If interpolation result is NaN use nearest neighbor
+        if np.isnan(conductance):
+            conductance = griddata(points, values, (temperature, salinity), method='nearest')
+        
     else:
         conductance = value['Conductance 1/(ohm.cm)'].values[0] 
     
