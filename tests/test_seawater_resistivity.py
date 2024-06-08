d85c376a669994b95ef48c2ea8313dbbaf3dcd39
samdansk2 <samdanshaik8998@gmail.com>
2024-06-08 10:36:20 +0530

    add_tests

--- tests/test_seawater_resistivity.py
+++ tests/test_seawater_resistivity.py
@@ -12,36 +12,28 @@ if seawater_dir not in sys.path:
 
 from src.seawater_resistivity import calculate_resistivity
 
-def sample_data():
-    data = {
-        'Temperature (deg C)': [0, 10, 20, 30],
-        'Salinity (%)': [0, 1, 2, 3],
-        'Conductance 1/(ohm.cm)': [1, 2, 3, 4]
-    }
-    return pd.DataFrame(data)
+def test_linear_interpolation():
+    assert calculate_resistivity(15, 1.5) == 1 / 2.5
 
-def test_linear_interpolation(sample_data):
-    assert calculate_resistivity(15, 1.5, sample_data) == 1 / 2.5
+def test_nearest_neighbor_interpolation():
+    assert calculate_resistivity(100, 100) == 1 / 4
+    assert calculate_resistivity(-100, -100) == 1 / 1
 
-def test_nearest_neighbor_interpolation(sample_data):
-    assert calculate_resistivity(100, 100, sample_data) == 1 / 4
-    assert calculate_resistivity(-100, -100, sample_data) == 1 / 1
+def test_edge_cases():
+    assert calculate_resistivity(0, 0) == 1
+    assert calculate_resistivity(30, 3) == 1 / 4
 
-def test_edge_cases(sample_data):
-    assert calculate_resistivity(0, 0, sample_data) == 1
-    assert calculate_resistivity(30, 3, sample_data) == 1 / 4
+def test_zero_temperature():
+    assert calculate_resistivity(0, 1) == 1 / 1
+    assert calculate_resistivity(0, 2) == 1 / 1.5
 
-def test_zero_temperature(sample_data):
-    assert calculate_resistivity(0, 1, sample_data) == 1 / 1
-    assert calculate_resistivity(0, 2, sample_data) == 1 / 1.5
+def test_zero_salinity():
+    assert calculate_resistivity(20, 0) == 1 / 2
+    assert calculate_resistivity(10, 0) == 1 / 1
 
-def test_zero_salinity(sample_data):
-    assert calculate_resistivity(20, 0, sample_data) == 1 / 2
-    assert calculate_resistivity(10, 0, sample_data) == 1 / 1
-
-def test_zero_temperature_and_salinity(sample_data):
-    assert calculate_resistivity(0, 0, sample_data) == 1
-    assert calculate_resistivity(0, 0.1, sample_data) == 1 / 1.1
+def test_zero_temperature_and_salinity():
+    assert calculate_resistivity(0, 0) == 1
+    assert calculate_resistivity(0, 0.1) == 1 / 1.1
 
 
 
