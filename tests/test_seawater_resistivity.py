faad3dafb7670780b2252447b6601c2555781f2a
samdansk2 <samdanshaik8998@gmail.com>
2024-06-25 09:01:03 +0530

    update_tests

--- tests/test_seawater_resistivity.py
+++ tests/test_seawater_resistivity.py
@@ -13,9 +13,10 @@ if seawater_dir not in sys.path:
 from src.seawater_resistivity import calculate_resistivity
 
 def test_non_df_value():
+    expected_result = 16.857
     temperature = 30  
     salinity = 4.0    
-    assert calculate_resistivity(temperature, salinity) == 16.857
+    assert calculate_resistivity(temperature, salinity) == expected_result
 
 def test_negative_value():
     temperature = 10
@@ -28,19 +29,22 @@ def test_negative_value():
         raise AssertionError("Value error not for non negative values.")
 
 def test_zero_temp():
+    expected_result = 33.462
     temperature = 0   
     salinity = 2    
-    assert calculate_resistivity(temperature, salinity) == 33.462
+    assert calculate_resistivity(temperature, salinity) == expected_result
 
 def test_random_input():
+    expected_result = 18.672
     temperature = 20  
     salinity = 3.0    
-    assert calculate_resistivity(temperature, salinity) == 18.672
+    assert calculate_resistivity(temperature, salinity) == expected_result
 
 def test_with_zero():  
+    expected_result = 543.774
     temperature = 0   
     salinity = 0    
-    assert calculate_resistivity(temperature, salinity) == 543.774
+    assert calculate_resistivity(temperature, salinity) == expected_result
 
     print("All tests pass!")
 
